991f3af027b96810226b63f3c2d2fd2f02cfa1cc
GitDarren <darren321@gmail.com>
2017-11-26 20:18:17 -0600

    creating folders and files for the game

---
+++ assets/javascript/app.js
@@ -0,0 +1,29 @@
+console.log("Hello World");
+
+
+
+//Pseudocode for the What the Hell We've learned so Far Trivia Game//
+//Create a Start button that starts the game.  The demo video shows the whole game is hidden until the start game button is pushed//
+
+
+
+
+//Start function - Button with an on "click"
+
+
+
+//Countdown Clock function - this function would also act as a game end function so that when the time reaches zero, the game stops, the guesses are tallied
+
+
+
+
+//Create a Question function
+//Create an Answers function - four choices that can be either radio dials or buttons.  I think I like buttons better//
+
+//Create a way to replicate the question and answer function to offer more questions. 
+
+//Correct answer function - fills in how many correct answers were guessed
+
+//Incorrect answer function - fills in how many incorrect answers were guessed
+
+//Unanswered function - tallies how many questions the user did not guess. 
