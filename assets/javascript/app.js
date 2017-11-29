e7222226a21398581f1c69339b2a1617c8892b9f
GitDarren <darren321@gmail.com>
2017-11-29 10:38:32 -0600

    Building timer and question and answer section

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,85 @@
-console.log("Hello World");
 
-//Write Document.ready function to load the game
 
-var questionsArray = [{
-    question: "Does this damn thing worK?",
-    answersArray: ["Yes", "No", "WTF", "Go learn something else"],
+var questionsArray = [
+    {
+    question: "Who Won the 2016 World Series?",
+    answersArray: ["Chicago Cubs", "Chicago White Sox", "Cleveland Indias", "New York Yankess"],
+    correctAnswer: 0
+    },
+    {
+    question: "Who Managed the 2016 World Series?",
+    answersArray: ["John Madden", "Joe Madden", "Terry Francona", "Joe Girardi"],
     correctAnswer: 1
-}];
+    },
+    {
+    question: "Who Should have won the 2011 World Series?",
+    answersArray: ["the fucking St. Louis Cardinals", "New York Yankees", "Los Angeles Dodgers", "Of course the Texas Rangers you insenstive asshole"],
+    correctAnswer: 3
+    }
+];
 
 $("document").ready(function () {
     $("#endGameButton").hide();
-
-
+    makeQuestionsSection();
 
     //Put Global Variables Here
 
-    //Pseudocode for the What the Hell We've learned so Far Trivia Game//
+    //Pseudocode for the World Series Trivia Game//
     //Create a Start button that starts the game.  The demo video shows the whole game is hidden until the start game button is pushed//
 
     //Start function - Button with an on "click"
     $("#startButton").on("click", function () {
-
         runGame();
     });
 
+    var seconds = 5;
+    var counter;
+
     var runGame = function () {
+        counter = setInterval(timer, 1000);
         //hide startbutton
         //show questions Div
         $("#startButton").hide();
-        // countdownClock();
         $("#questionsSection").show();
         $("#endGameButton").show();
     };
+
+    function timer() {
+        seconds--;
+        $('#countdownClock').text(seconds);
+        if (seconds === 0) {
+            clearInterval(counter);
+            return;
+        }
+        console.log("There are " + seconds + " seconds left");
+    }
+
+    function makeQuestionsSection() {
+        questionsArray.forEach(function(val, i) {
+            var options = questionsArray[i].answersArray;      
+            $("#questionsSection").append (
+            `<h2> ${questionsArray[i].question}</h2>`
+            
+            ) 
+            options.forEach(function(v, idx) {
+                $("#questionsSection").append (
+                    `<form> <input type='radio' name='choice' value=' ${options[idx]}'> ${options[idx]} <br></form>`
+                )
+            })
+        })
+        
+    };
+
+
+    $("#endGameButton").on("click", function () {
+        clearInterval(counter);
+        return;
+        //end game with click of the button.
+
+    });
+
+});
+
 
     //Create new function to populate questionsSection with questionsArray
     //write for loop that will loop through questionArray at each index
@@ -41,14 +89,6 @@ $("document").ready(function () {
     //figure out how I want the questionsSection to be displayed 
     //figure out how I want the answersAarry to be display? - Possibly Buttons
 
-
-
-    $("#endGameButton").on("click", function () {
-        //end game with click of the button.
-
-    });
-
-});
 
 //Countdown Clock function - this function would also act as a game end function so that when the time reaches zero, the game stops, the guesses are tallied
 
